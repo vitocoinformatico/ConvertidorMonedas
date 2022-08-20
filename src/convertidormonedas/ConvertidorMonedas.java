a1fcb452048fc1892c9b37816f4db53c3e02ee90
Victor_Saavedra <saavedra_victor1984@hotmail.com>
2022-08-20 00:19:47 -0400

    Se crea ventana de menu incial del proyecto

--- src/convertidormonedas/ConvertidorMonedas.java
+++ src/convertidormonedas/ConvertidorMonedas.java
@@ -1,8 +1,27 @@
 package convertidormonedas;
 
+import javax.swing.JOptionPane;
+
 public class ConvertidorMonedas {
 
     public static void main(String[] args) {
+        
+        String opcionElejida;
+        
+//        Array de Objectos
+        String[] menu =  {
+            "Seleccione...",
+            "Conversor de Moneda"
+        };
+        
+        opcionElejida = (String) JOptionPane.showInputDialog(null,
+                "Seleccione una opción de conversión",
+                "Menu",
+                JOptionPane.DEFAULT_OPTION,
+                null,
+                menu,
+                menu[0]
+        );
         
     }
 
